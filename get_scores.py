4bfe5027a30afe8f4957260efff7d414d173d6a9
Lucas Murtinho <lucasmurtinho@gmail.com>
2022-12-16 18:33:52 -0300

    using sklearn.euclidean_distances to calculate distances between vectors in the data

--- get_scores.py
+++ get_scores.py
@@ -2,29 +2,23 @@ from sklearn.metrics import silhouette_score
 from sklearn.metrics import calinski_harabasz_score
 from sklearn.metrics import davies_bouldin_score
 from sklearn.metrics import normalized_mutual_info_score as nmi
+from sklearn.metrics.pairwise import euclidean_distances
 import networkx as nx
 import numpy as np
 
-def get_dist(x, y):
-    return np.linalg.norm(x - y)
+def get_min_dist(dists, labels, c1, c2):
+  wd = dists[labels==c1][:,labels==c2]
+  return wd.min()
 
-def get_min_dist(data, labels, l1, l2):
-  n, _ = data.shape
-  return min([get_dist(data[i], data[j]) 
-              for i in range(n)
-              for j in range(n)
-              if labels[i] == l1
-              and labels[j] == l2])
-
-def get_all_dists(data, labels):
+def get_all_dists(vector_dists, labels):
   unique_labels = np.unique(labels)
-  k = len(unique_labels)
-  dists = np.zeros((k, k))
-  for i in unique_labels:
-    for j in unique_labels:
+  n_labels = len(unique_labels)
+  ans = np.zeros((n_labels, n_labels))
+  for i in range(n_labels):
+    for j in range(i, n_labels):
       if i != j:
-        dists[i, j] = dists[j, i] = get_min_dist(data, labels, i, j)
-  return dists
+        ans[i,j] = ans[j,i] = get_min_dist(vector_dists, labels, i, j)
+  return ans
 
 def get_mst_edges(dists):
   g = nx.from_numpy_array(dists)
@@ -38,7 +32,8 @@ def get_graph_edges(dists):
   return g.edges.data()
 
 def get_scores(data, labels, true_labels=None):
-  dists = get_all_dists(data, labels)
+  vector_dists = euclidean_distances(data)
+  dists = get_all_dists(vector_dists, labels)
   graph_edges = get_graph_edges(dists)
   mst_edges = get_mst_edges(dists)
   mst_weights = [edge[2]['weight'] for edge in mst_edges]

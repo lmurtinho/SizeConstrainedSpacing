5c24345738eb5c057ea1b09387c4a61ba0877d63
Lucas Murtinho <lucas.murtinho@gmail.com>
2023-05-17 11:31:13 -0300

    added: generate_models can run for several algorithms

--- generate_models.py
+++ generate_models.py
@@ -2,15 +2,14 @@ import os
 import joblib
 import numpy as np
 import pandas as pd
-import time
 from datetime import datetime
 import argparse
+import time
 
 import warnings
 warnings.filterwarnings('ignore')
 
 from get_scores import get_scores
-from helper import get_model, single_model_result
 from constrained import ConstrainedMaxMST
 from feasible import FeasibleSpacing
 from sklearn.cluster import KMeans, AgglomerativeClustering
@@ -24,8 +23,40 @@ DATASET_LIST = ['anuran', 'avila', 'collins', 'digits',
                 'iris', 'letter', 'mice', 'newsgroups', 
                 'pendigits', 'sensorless', 'vowel']
 
-def retrieve_results(algo, dataset_list, min_size_factor, km_filepath, sl_filepath,
-                     seed=None, search_all=True, verbose=False):
+def get_model(dataset, filepath, single=False):
+    df = joblib.load(filepath)
+    if single:
+        return df[df.dataset==dataset].model.iloc[0]
+    else:
+        return df[df.dataset==dataset].model
+
+def single_model_result(model, algo, data,
+                        min_size=None,
+                        sl_model=None,
+                        verbose=False):
+    start = time.time()
+    if algo in ['cm_full', 'cm_fast', 'fs'] and sl_model:
+        labels = model.fit_predict(data, sl_model)
+    else:
+        labels = model.fit_predict(data)
+    end = time.time()
+    secs = end - start
+    if verbose:
+        message = f'\tModel {algo} done in {secs:.2f} seconds'
+        if algo in ['cm_full', 'cm_fast', 'fs']:
+            message += f' for min_size {min_size}'
+        print(message)
+    res = {'model': model,
+           'algorithm': algo,
+           'n': len(data),
+           'time': secs,
+           'smallest_cluster': np.bincount(labels).min(),
+           'min_size': min_size,
+            }
+    return res
+
+def retrieve_results(algo, dataset_list, min_size_factor=None,
+                     km_filepath=None, sl_filepath=None, verbose=False):
 
     results = []
 
@@ -37,31 +68,38 @@ def retrieve_results(algo, dataset_list, min_size_factor, km_filepath, sl_filepa
         data, k = get_dataset(dataset)
         
         # get min_sizes
-        if args.algo in ['cm', 'fs'] and km_filepath:
+        if algo in ['cm_full', 'cm_fast', 'fs'] and km_filepath:
             km_models = get_model(dataset, km_filepath, False)
             min_sizes = km_models.apply(lambda x: np.bincount(x.labels_).min()).values
-            min_sizes = (min_sizes / min_size_factor + 1).astype(int)
+            min_sizes = (min_sizes // min_size_factor + 1).astype(int)
         else:
-            min_sizes = [min_size_factor * len(data) / k]
+            min_sizes = [min_size_factor * len(data) // k]
         
         # get sl_model
-        if args.algo in ['cm', 'fs'] and sl_filepath:
+        if algo in ['cm_full', 'cm_fast', 'fs'] and sl_filepath:
             sl_model = get_model(dataset, sl_filepath, True)
         else:
             sl_model = None
         
-        if algo in ['cm', 'fs']:
+        if algo in ['cm_full', 'cm_fast', 'fs']:
             rng_runs = min_sizes
         elif algo == 'km':
             rng_runs = range(1,11)
         elif algo == 'sl':
-            rng_runs == [1]
+            rng_runs = [1]
+
+        if algo == 'cm_full':
+            search_all = True
+        elif algo == 'cm_fast':
+            search_all = False
+        else:
+            search_all = None
         
         res_dict = {}
         for i in range(len(rng_runs)):
             
             # get min_size and theck if model already ran
-            if algo in ['cm', 'fs']:
+            if algo in ['cm_full', 'cm_fast', 'fs']:
                 min_size = rng_runs[i]
                 if min_size in res_dict:
                     res = res_dict[min_size]
@@ -71,7 +109,7 @@ def retrieve_results(algo, dataset_list, min_size_factor, km_filepath, sl_filepa
                 min_size = None
 
             # select model
-            if algo=='cm':
+            if algo in ['cm_full', 'cm_fast']:
                 seed = 1
                 model = ConstrainedMaxMST(k, min_size, random_state=seed, search_all=search_all)
             if algo=='fs':
@@ -83,17 +121,20 @@ def retrieve_results(algo, dataset_list, min_size_factor, km_filepath, sl_filepa
                 model = AgglomerativeClustering(k, linkage='single')
             
             # fit model and predict labels          
-            res = single_model_results(algo, data, min_size, 
+            res = single_model_result(model, algo, data, min_size,
                                        sl_model, verbose) 
+            res['dataset'] = dataset
+            res['k'] = k
+            res['id'] = i + 1
             score = get_scores(data, res['model'].labels_)
             res = res | score
             if algo == 'km':
                 res['seed'] = rng_runs[i]
-            if algo == 'cm':
+            if algo in ['cm_full', 'cm_fast']:
                 res['seed'] = 1
                 res['min_dist_upper_bound'] = model.min_dist_sum,
                 res['search_all'] = search_all
-            if algo in ['cm', 'fs']:
+            if algo in ['cm_full', 'cm_fast', 'fs']:
                 res['min_size_factor'] = min_size_factor
                 res_dict[min_size] = res
             results.append(res)
@@ -102,48 +143,44 @@ def retrieve_results(algo, dataset_list, min_size_factor, km_filepath, sl_filepa
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-a', '--algo', default='cm', choices=['cm', 'fs', 'km', 'sl'],
-                    help='algorithm to be used for clustering')
+parser.add_argument('-a', '--algos', default=['sl', 'km', 'fs', 'cm_full', 'cm_fast'],
+                    help='algorithms to be used for clustering')
 parser.add_argument('-dl', '--dataset_list', nargs='+', default=DATASET_LIST,
                     help="list of datasets for which models will be generated")
 parser.add_argument('-hf', '--hour_in_filename', action='store_true',
                     help='adds to the filename the time of file creation')
 parser.add_argument('-rp', '--results_path', default=RESULTS_PATH,
                     help="path in which results will be saved; will be created if it doesn't exist")
-parser.add_argument('-sp', '--sl_filepath', default=f'{MODELS_PATH}/results_SL.joblib',
+parser.add_argument('-sp', '--sl_filepath', default=f'{MODELS_PATH}/results_sl.joblib',
                     help="path to file with single-link models")
-parser.add_argument('-kp', '--km_filepath', default=f'{MODELS_PATH}/results_KM.joblib',
+parser.add_argument('-kp', '--km_filepath', default=f'{MODELS_PATH}/results_km.joblib',
                     help="path to file with k-means models")
 parser.add_argument('-ms', '--min_size_factor', type=float, default=0.75,
                     help='for algo in [cm, fs]: size factor of minimum size guarantee')
-parser.add_argument('-sa', '--search_all', action='store_true',
-                    help="for algo = cm: searches through all possible partitions from 2 to k for each model")
 parser.add_argument('-v', '--verbose', action='store_true',
                     help='prints progress information')
+parser.add_argument('-sm', '--save_models', action="store_true",
+                    help="saves a .joblib file with the fitted models")
 
 if __name__ == '__main__':
     args = parser.parse_args()
     
-    # Get results
-    if args.verbose:
-        print('finding models for algorithm', args.algo)
-        if args.algo == 'cm':
-            print('search_all:', args.search_all)
-    results = retrieve_results(args.algo, args.dataset_list, args.min_size_factor, args.km_filepath, 
-                               args.sl_filepath, args.search_all, args.verbose)
-    full_results = pd.DataFrame(results)
+    for algo in args.algos:
     
-    # Save results
-    os.makedirs(args.results_path, exist_ok=True)
-    if args.hour_in_filename:
-        now = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M')
-        filename = f'{args.results_path}/{now}_results_{args.algo}'
-    else:
-        filename = f'{args.results_path}/results_{args.algo}'
-    if args.algo == 'cm':
-        if args.search_all:
-            filename += '_exhaustive'
+        # Get results
+        if args.verbose:
+            message = f'finding models for algorithm {algo}'
+        results = retrieve_results(algo, args.dataset_list, args.min_size_factor, args.km_filepath,
+                                   args.sl_filepath, args.verbose)
+        full_results = pd.DataFrame(results)
+
+        # Save results
+        os.makedirs(args.results_path, exist_ok=True)
+        if args.hour_in_filename:
+            now = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M')
+            filepath = f'{args.results_path}/{now}_results_{algo}'
         else:
-            filename += '_fast'
-    joblib.dump(full_results, f'{filename}.joblib')
-    full_results.drop(columns=['model']).to_csv(f'{filename}.csv', index=False)
+            filepath = f'{args.results_path}/results_{algo}'
+        if args.save_models:
+            joblib.dump(full_results, f'{filepath}.joblib')
+        full_results.drop(columns=['model']).to_csv(f'{filepath}.csv', index=False)
